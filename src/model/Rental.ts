84dd237147ec8921f1e0dd29663885f5407598ce
Till <till2000@live.com>
2023-06-07 13:56:57 +0200

    feat(rental): add static method

--- src/model/Rental.ts
+++ src/model/Rental.ts
@@ -1,5 +1,7 @@
-import { Document, model, Schema } from 'mongoose';
+import { Document, Model, model, Schema } from 'mongoose';
 import { Joi } from 'utils/validation';
+import { ObjectId } from 'mongodb';
+import moment from 'moment';
 
 export type Rental = {
   _id: string;
@@ -17,70 +19,95 @@ export type Rental = {
   rentalFee?: number;
 };
 
-export type RentalDocument = Document<unknown, {}, Rental> &
-  Omit<Rental & Required<{ _id: string }>, never>;
-
-export type RequestRental = {
-  customerId: string;
-  movieId: string;
+type RentalMethods = {
+  return: () => void;
 };
 
-export const Rental = model<Rental>(
-  'Rental',
-  new Schema({
-    customer: {
-      type: new Schema({
-        name: {
-          type: String,
-          required: true,
-          minlength: 5,
-          maxlength: 50,
-        },
-        isGold: {
-          type: Boolean,
-          default: false,
-        },
-        phone: {
-          type: String,
-          required: true,
-          minlength: 5,
-          maxlength: 50,
-        },
-      }),
-      required: true,
-    },
-    movie: {
-      type: new Schema({
-        title: {
-          type: String,
-          required: true,
-          trim: true,
-          minlength: 5,
-          maxlength: 255,
-        },
-        dailyRentalRate: {
-          type: Number,
-          required: true,
-          min: 0,
-          max: 255,
-        },
-      }),
-      required: true,
-    },
-    dateOut: {
-      type: Date,
-      required: true,
-      default: Date.now,
-    },
-    dateReturned: {
-      type: Date,
-    },
-    rentalFee: {
-      type: Number,
-      min: 0,
-    },
-  })
-);
+interface RentalModel extends Model<Rental, {}, RentalMethods> {
+  lookup(customerId: ObjectId, movieId: ObjectId): RentalDocument;
+}
+
+export type RentalDocument = Document<unknown, {}, Rental> &
+  Omit<Rental & Required<{ _id: string }>, 'return'> &
+  RentalMethods;
+
+export type RequestRental = {
+  customerId: ObjectId;
+  movieId: ObjectId;
+};
+
+const rentalSchema = new Schema<Rental, RentalModel, RentalMethods>({
+  customer: {
+    type: new Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 255,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+
+rentalSchema.statics.lookup = async function (
+  customerId: ObjectId,
+  movieId: ObjectId
+) {
+  return this.findOne({
+    'customer._id': customerId,
+    'movie._id': movieId,
+  });
+};
+
+rentalSchema.methods.return = function () {
+  this.dateReturned = new Date();
+
+  const rentalDays = moment().diff(this.dateOut, 'days');
+  this.rentalFee = rentalDays * this.movie.dailyRentalRate;
+};
+
+export const Rental = model<Rental, RentalModel>('Rental', rentalSchema);
 
 export const validateRental = (rental: RequestRental) => {
   const schema = Joi.object<RequestRental>({
--- src/router/ReturnsRouter/ReturnsRouter.ts
+++ src/router/ReturnsRouter/ReturnsRouter.ts
@@ -1,7 +1,6 @@
 import express, { Response } from 'express';
 import { Movie, Rental, Request, Return, validateReturn } from 'model';
 import { BodyHandler, SecurityHandler } from 'handler';
-import moment from 'moment';
 
 export const router = express.Router();
 
@@ -9,18 +8,14 @@ router.post(
   '/',
   [SecurityHandler, BodyHandler(validateReturn)],
   async (req: Request<Return>, res: Response<Rental | string>) => {
-    const rental = await Rental.findOne({
-      'customer._id': req.body.customerId,
-      'movie._id': req.body.movieId,
-    });
+    const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
     if (!rental) return res.status(404).send('Rental not found.');
 
     if (rental.dateReturned)
       return res.status(400).send('Return already processed.');
 
-    rental.dateReturned = new Date();
-    const rentalDays = moment().diff(rental.dateOut, 'days');
-    rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+    rental.return();
+    await rental.save();
 
     await Movie.updateOne(
       { _id: (rental.movie as Movie)._id },
@@ -28,6 +23,6 @@ router.post(
     );
     await rental.save();
 
-    return res.status(200).send(rental);
+    return res.send(rental);
   }
 );
